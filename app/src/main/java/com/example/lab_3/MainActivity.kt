a4b17fb65e960f0016d32ff0644c36e9ccc776d5
emil_ <emil.kushner.stud@mail.ru>
2023-09-28 00:30:40 +0700

    Declaring variables

--- app/src/main/java/com/example/lab_3/MainActivity.kt
+++ app/src/main/java/com/example/lab_3/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.lab_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonToResult = findViewById<Button>(R.id.buttonToResult)
+        val inputString = findViewById<EditText>(R.id.inputString)
+        val charOff = findViewById<EditText>(R.id.inputCharOff)
+        val charOn = findViewById<EditText>(R.id.inputCharOn)
+        val result = findViewById<EditText>(R.id.resultText)
     }
 }
